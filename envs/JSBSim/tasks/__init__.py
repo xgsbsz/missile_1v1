27ad8e52d35e024028eef6ced8ee5253e236c5f9
xgsbsz <samhappyforyou@126.com>
2023-05-12 23:00:53 +0800

    2

--- envs/JSBSim/tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-from .heading_task import HeadingTask
-from .singlecombat_task import SingleCombatTask, HierarchicalSingleCombatTask
-from .singlecombat_with_missle_task import SingleCombatDodgeMissileTask, HierarchicalSingleCombatDodgeMissileTask, HierarchicalSingleCombatShootTask, SingleCombatShootMissileTask
-from .intercept_task import InterceptTask
